182ac6a6cd8f50e004f4e6d1fa40b4a6ec48b7bf
batsauto <cfbarrow@gmail.com>
2017-01-03 18:57:34 -0800

    added the datepicker for choosing a date on the repair form. I ran into an issue where the datepicker wasnt showing the dates so I found a workaround and implamented it into the app.js file.

--- client/app.js
+++ client/app.js
@@ -1,10 +1,15 @@
 var myAppModule = angular.module("myApp", ["ngRoute", "vsGoogleAutocomplete", "ngMaterial", "angular.filter"]);
 
-myAppModule.config(function($mdThemingProvider) {
-  $mdThemingProvider.theme('default')
-    //.dark();
-    .primaryPalette('blue')
-    .accentPalette('teal')
-    .warnPalette('red')
-    .backgroundPalette('grey');
-});
+// This config is so the datepicker directive can work in the app
+myAppModule.config(function($compileProvider) {
+    $compileProvider.preAssignBindingsEnabled(true);
+  });
+
+// myAppModule.config(function($mdThemingProvider) {
+//   $mdThemingProvider.theme('default')
+//     //.dark();
+//     .primaryPalette('blue')
+//     .accentPalette('teal')
+//     .warnPalette('red')
+//     .backgroundPalette('grey');
+// });
