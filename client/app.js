5ac44fc1bd6ac3ac11ef5046e4d6c35299d23ed4
Christian Barrow <cfbarrow@gmail.com>
2017-01-04 00:36:15 -0800

    I added the md-data-table library so I c ould display the current repairs with the angular material theme.

--- client/app.js
+++ client/app.js
@@ -1,9 +1,16 @@
-var myAppModule = angular.module("myApp", ["ngRoute", "vsGoogleAutocomplete", "ngMaterial", "angular.filter"]);
+var myAppModule = angular.module("myApp", ["ngRoute", "vsGoogleAutocomplete", "ngMaterial", "angular.filter", "md.data.table"]);
 
 // This config is so the datepicker directive can work in the app
 myAppModule.config(function($compileProvider) {
     $compileProvider.preAssignBindingsEnabled(true);
   });
+
+
+// myAppModule.config(function($mdDateLocaleProvider) {
+//     $mdDateLocaleProvider.formatDate = function(date) {
+//       return moment(date).format('MM-DD-YYYY');
+//     };
+//   });
 
 // myAppModule.config(function($mdThemingProvider) {
 //   $mdThemingProvider.theme('default')
