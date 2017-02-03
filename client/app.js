bad8572f9c2f1bcaed9c54afb24a59b2ca6ca43b
Christian Barrow <cfbarrow@gmail.com>
2017-02-02 23:37:58 -0800

    reformatted tabs views and got the parts displaying correctly. Also updated angular material to 1.1.3

--- client/app.js
+++ client/app.js
@@ -1,9 +1,9 @@
 var myAppModule = angular.module("myApp", ["ngRoute", "vsGoogleAutocomplete", "ngMaterial", "angular.filter", "ngAnimate", "ngMdIcons", "ngAvatar"]);
 
-// This config is so the datepicker directive can work with this version of anuglar
-myAppModule.config(function($compileProvider) {
-    $compileProvider.preAssignBindingsEnabled(true);
-  });
+// This config is so the datepicker directive can work with this version of anuglar-----Issue was resolved with angular material 1.1.3  no need for this block anymore.
+// myAppModule.config(function($compileProvider) {
+//     $compileProvider.preAssignBindingsEnabled(true);
+//   });
 
 
 myAppModule.config(function($mdThemingProvider) {
