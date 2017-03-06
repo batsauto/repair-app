2d38c2c8eb9842f633509e8c30847688566aa4c6
Christian Barrow <cfbarrow@gmail.com>
2017-03-06 09:06:19 -0800

    cleaned up my partFactory to give it one responsability

--- client/js/factories/partFactory.js
+++ client/js/factories/partFactory.js
@@ -1,35 +1,4 @@
 myAppModule.factory("partFactory", function($http) {
-    // var parts = [
-    //     "1804 Sprinkler",
-    //     "1806 Sprinkler",
-    //     "MP Rotator Nozzle",
-    //     "Nozzle",
-    //     "Tree Lodge Pole",
-    //     "Tree Ties",
-    //     "1 inch DVF Valve",
-    //     "1 inch valve part kit",
-    //     "2 inch DVF Valve",
-    //     "Gel Cap",
-    //     "Shrub Adapter",
-    //     "3/4 inch Galvenized Pipe (ft)",
-    //     "3/4 inch PVC Pipe Class (ft)",
-    //     "3/4 inch PVC Pipe Sch 40 (ft)",
-    //     "3/4 inch PVC Pipe Class (ft)",
-    //     "1 inch PVC Pipe Class (ft)",
-    //     "1 1/2 inch ball valve"
-    // ];
-
-    // var partsPerRepairs = [
-    //   {
-    //     quantity: "1",
-    //     part: "Gel Cap"
-    //   },
-    //   {
-    //     quantity: "3",
-    //     part: "Shrub Adapter"
-    //   }
-    // ];
-
     const factory = {};
 
     factory.getParts = getParts;
@@ -49,19 +18,6 @@ myAppModule.factory("partFactory", function($http) {
             return { success: false, message: error };
         };
     }
-
-    // factory.getPartsPerRepair = function(callback) {
-    //     callback(partsPerRepairs);
-    // };
-    //
-    // factory.addPartsPerRepair = function(newPartsPerRepair) {
-    //     partsPerRepairs.push(newPartsPerRepair);
-    // };
-    //
-    // factory.removePartsPerRepair = function($index) {
-    //     partsPerRepairs.splice($index, 1);
-    // };
-
 
     return factory;
 
