5e9b5c3649ae1d43cd6c7eb3dfbd17895c6ba363
Christian Barrow <cfbarrow@gmail.com>
2017-03-14 14:17:53 -0700

    refactored using J.Papa style guide

--- client/js/controllers/repairsController.js
+++ client/js/controllers/repairsController.js
@@ -1,11 +1,73 @@
-myAppModule.controller("repairsController", ['$scope', '$mdSidenav', '$mdDialog', '$mdMedia', 'repairFactory', 'partFactory', 'NgMap', function($scope, $mdSidenav, $mdDialog, $mdMedia, repairFactory, partFactory, NgMap) {
+myAppModule.controller("repairsController", ['$scope', '$mdSidenav', '$mdDialog', '$mdMedia', 'repairFactory', 'partFactory', 'NgMap', function ($scope, $mdSidenav, $mdDialog, $mdMedia, repairFactory, partFactory, NgMap) {
 
+    $scope.addRepair = addRepair;
+    $scope.deleteRepairDialog = deleteRepairDialog;
+    $scope.editRepair = editRepair;
+    $scope.refreshRepairs = refreshRepairs;
+    $scope.removeRepair = removeRepair;
     $scope.selected = null;
+    $scope.selectRepair = selectRepair;
     $scope.searchCustomer = "";
+    $scope.sendEmail = sendEmail;
     $scope.tabIndex = 0;
+    $scope.toggleSideNav = toggleSideNav;
 
     fetchRepairs();
     fetchParts();
+    NgMap.getMap().then(function (map) {
+    });
+
+    function addRepair($event) {
+        var useFullScreen = ($mdMedia('sm') || $mdMedia('xs'));
+        $mdDialog.show({
+            templateUrl: './static/partials/addRepairDialog.html',
+            parent: angular.element(document.body),
+            targetEvent: $event,
+            controller: "addRepairDialogController",
+            clickOutsideToClose: true,
+            fullscreen: useFullScreen
+        }).then(function () {
+            $scope.newRepair = {};
+            fetchRepairs();
+        }, function () {
+            console.log("You Cancelled the Dialog");
+        });
+    }
+
+    function deleteRepairDialog($event) {
+        var confirm = $mdDialog.confirm()
+            .title("Are you sure you want to delete " + $scope.selected.fullName)
+            .textContent("This action can not be undone!")
+            .ariaLabel("Confirm Delete Repair")
+            .targetEvent($event)
+            .ok("Yes")
+            .cancel("No");
+
+        $mdDialog.show(confirm).then(function () {
+            removeRepair();
+        }, function () {
+            console.log("You cancelled the Delete Dialog");
+        });
+    }
+
+    function editRepair($event) {
+        var useFullScreen = ($mdMedia('sm') || $mdMedia('xs'));
+        $mdDialog.show({
+            scope: $scope.$new(),
+            preserveScope: true,
+            templateUrl: './static/partials/editRepairDialog.html',
+            parent: angular.element(document.body),
+            targetEvent: $event,
+            controller: 'editRepairDialogController',
+            clickOutsideToClose: true,
+            fullscreen: useFullScreen
+        }).then(function () {
+            $scope.refreshRepairs();
+        }, function () {
+            $scope.refreshRepairs();
+            console.log("You Cancelled the Dialog");
+        });
+    }
 
     function fetchRepairs() {
         repairFactory.getRepairs().then(function (repairs) {
@@ -24,11 +86,7 @@ myAppModule.controller("repairsController", ['$scope', '$mdSidenav', '$mdDialog'
         });
     }
 
-    $scope.toggleSideNav = function() {
-      $mdSidenav("left").toggle();
-    };
-
-    $scope.selectRepair = function (repair) {
+    function selectRepair(repair) {
         $scope.selected = repair;
         repairFactory.selectedRepair = repair;
         var sidenav = $mdSidenav('left');
@@ -36,87 +94,35 @@ myAppModule.controller("repairsController", ['$scope', '$mdSidenav', '$mdDialog'
             sidenav.close();
         }
         $scope.tabIndex = 0;
-    };
+    }
 
-    $scope.addRepair = function($event) {
-        var useFullScreen = ($mdMedia('sm') || $mdMedia('xs'));
-        $mdDialog.show( {
-            templateUrl: './static/partials/addRepairDialog.html',
-            parent: angular.element(document.body),
-            targetEvent: $event,
-            controller: "addRepairDialogController",
-            clickOutsideToClose: true,
-            fullscreen: useFullScreen
-        }).then(function () {
-            $scope.newRepair = {};
-            fetchRepairs();
-        }, function() {
-            console.log("You Cancelled the Dialog");
-        });
-    };
-
-    $scope.editRepair = function($event) {
-        var useFullScreen = ($mdMedia('sm') || $mdMedia('xs'));
-        $mdDialog.show( {
-            scope: $scope.$new(),
-            preserveScope: true,
-            templateUrl: './static/partials/editRepairDialog.html',
-            parent: angular.element(document.body),
-            targetEvent: $event,
-            controller: 'editRepairDialogController',
-            clickOutsideToClose: true,
-            fullscreen: useFullScreen
-        }).then(function () {
-            $scope.refreshRepairs();
-        }, function() {
-            $scope.refreshRepairs();
-            console.log("You Cancelled the Dialog");
-        });
-    };
-
-    $scope.removeRepair = function() {
-        repairFactory.deleteRepair($scope.selected)
-        .then(function () {
-            fetchRepairs();
-        })
-    };
-
-    $scope.deleteRepairDialog = function($event) {
-        var confirm = $mdDialog.confirm()
-            .title("Are you sure you want to delete " + $scope.selected.fullName)
-            .textContent("This action can not be undone!")
-            .ariaLabel("Confirm Delete Repair")
-            .targetEvent($event)
-            .ok("Delete Repair")
-            .cancel("Cancel");
-
-        $mdDialog.show(confirm).then(function () {
-            $scope.removeRepair();
-        }, function () {
-            console.log("You cancelled the Delete Dialog");
-        });
-    };
-
-    $scope.sendEmail = function(email){
-        var link = "mailto:"+ email
-                    + "?subject=%20Hello%20" + $scope.selected.fullName + "%20from%20Barrow%27s%20Landscaping";
-        window.location.href = link;
-    };
-
-
-    $scope.refreshRepairs = function() {
+    function refreshRepairs() {
         var SelectedId = $scope.selected._id;
         repairFactory.getRepairs().then(function (repairs) {
             $scope.repairs = repairs;
-            for(var i=0; i<repairs.length; i++) {
+            for (var i = 0; i < repairs.length; i++) {
                 if (repairs[i]._id == SelectedId) {
                     $scope.selected = repairs[i];
                 }
             }
             repairFactory.selectedRepair = $scope.selected;
         });
-    };
+    }
 
-    NgMap.getMap().then(function(map) {
-    });
+    function removeRepair() {
+        repairFactory.deleteRepair($scope.selected)
+            .then(function () {
+                fetchRepairs();
+            })
+    }
+
+    function sendEmail(email) {
+        var link = "mailto:" + email
+            + "?subject=%20Hello%20" + $scope.selected.fullName + "%20from%20Barrow%27s%20Landscaping";
+        window.location.href = link;
+    }
+
+    function toggleSideNav() {
+        $mdSidenav("left").toggle();
+    }
 }]);
