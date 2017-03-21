8a9f43ca898f092084664fd0c4634be33977c431
Christian Barrow <cfbarrow@gmail.com>
2017-03-21 11:51:43 -0700

    refactored controller to follow J.Papa styleguide

--- client/js/controllers/editRepairDialogController.js
+++ client/js/controllers/editRepairDialogController.js
@@ -1,29 +1,40 @@
 myAppModule.controller("editRepairDialogController", [ '$scope', '$mdDialog', 'repairFactory', function($scope, $mdDialog, repairFactory) {
-    $scope.cancel = function() {
-        $mdDialog.cancel();
-    };
-    $scope.hide = function() {
-        $mdDialog.hide();
-    };
-    $scope.editRepair = function() {
-        repairFactory.updateRepair($scope.selected);
-        $scope.selectRepair($scope.selected);
-        $mdDialog.hide();
-    };
 
-    $scope.addPartsToRepair = function() {
+    $scope.addPartsToRepair = addPartsToRepair;
+    $scope.cancel = cancel;
+    $scope.editRepair = editRepair;
+    $scope.hide = hide;
+    $scope.noSundayPredicate = noSundayPredicate;
+    $scope.removePartsPerRepair = removePartsPerRepair;
+
+    function addPartsToRepair() {
         repairFactory.addPartsPerRepair($scope.selected, $scope.selected.newPartsPerRepair);
         $scope.selected.newPartsPerRepair = {};
         $scope.refreshRepairs();
-    };
+    }
 
-    $scope.removePartsPerRepair = function (partsPerRepair) {
-        repairFactory.removePartsFromRepair(partsPerRepair);
-        $scope.refreshRepairs();
-    };
+    function cancel() {
+        $mdDialog.cancel();
+    }
 
-    $scope.noSundayPredicate = function(date) {
+    function editRepair () {
+        repairFactory.updateRepair($scope.selected);
+        $scope.selectRepair($scope.selected);
+        $mdDialog.hide();
+    }
+
+    function hide () {
+        $mdDialog.hide();
+    }
+
+    function noSundayPredicate (date) {
         var day = date.getDay();
         return day !== 0;
-    };
+    }
+
+    function removePartsPerRepair (partsPerRepair) {
+        repairFactory.removePartsFromRepair(partsPerRepair);
+        $scope.refreshRepairs();
+    }
+
 }]);
